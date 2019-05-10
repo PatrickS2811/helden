c7c8c7418da29c8c4769b620c688bb7af59f37e5
PatrickS2811 <43434529+PatrickS2811@users.noreply.github.com>
2019-05-10 22:06:51 +0200

    Add files via upload

---
+++ helden/js/helden.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+// test of het document werkt.
+// $('main').hide(3000).delay(1000).show(500);
+
+// muis events.
+$('figure img').mouseenter( function(){ 
+	$(this).fadeTo(600, .01);
+}).mouseleave(function(){
+	$(this).fadeTo(1000, 1);
+}).click(function(){
+	$(this).next().toggle(700);
+});
+
+//footer
+
+$('footer').click ( function(){
+	$(this).animate( {
+		top: '-=50px',
+		opacity: '-=0.2'
+	}, 900)
+	}).dblclick( function(){
+		$(this).animate({
+			top: '0',
+			opacity: '1'
+		}, 900)
+	})
+});
+
